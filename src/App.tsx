d24dc8174aaefbc9816098889fb4fcbdd27a3572
Takahiro Miyoshi <takahiro-miyoshi@sankichi.net>
2024-12-22 15:08:47 +0900

    Setup react-map-gl

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,17 @@
-function App() {
-  return <h1>react-sat-map</h1>;
-}
+import { Map } from "react-map-gl/maplibre";
 
-export default App;
+import "maplibre-gl/dist/maplibre-gl.css";
+
+export default function App() {
+  return (
+    <Map
+      initialViewState={{
+        longitude: 139.78881425,
+        latitude: 35.637313872,
+        zoom: 1.5,
+      }}
+      style={{ height: "100vh" }}
+      mapStyle="https://tile.openstreetmap.jp/styles/maptiler-basic-ja/style.json"
+    />
+  );
+}
