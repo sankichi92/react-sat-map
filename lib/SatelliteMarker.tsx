927f1eb76f2c5a803d358bd784bf4ab9ff9dab4c
Takahiro Miyoshi <takahiro-miyoshi@sankichi.net>
2024-12-22 22:49:29 +0900

    Enable to show popup

--- lib/SatelliteMarker.tsx
+++ lib/SatelliteMarker.tsx
@@ -1,5 +1,6 @@
-import { useMemo } from "react";
-import { Marker } from "react-map-gl/maplibre";
+import { useCallback, useMemo, useState } from "react";
+import type { MarkerProps } from "react-map-gl/maplibre";
+import { Marker, Popup } from "react-map-gl/maplibre";
 import type { GMSTime } from "satellite.js";
 import {
   degreesLat,
@@ -22,6 +23,13 @@ export function SatelliteMarker({
   date = new Date(),
   gmst = gstime(date),
 }: SatelliteMarkerProps) {
+  const [showPopup, setShowPopup] = useState(false);
+
+  const onSatClick: NonNullable<MarkerProps["onClick"]> = useCallback((e) => {
+    e.originalEvent.stopPropagation();
+    setShowPopup(true);
+  }, []);
+
   const satrec = useMemo(() => {
     return twoline2satrec(satellite.tle.line1, satellite.tle.line2);
   }, [satellite]);
@@ -34,14 +42,42 @@ export function SatelliteMarker({
   }
 
   const location = eciToGeodetic(position, gmst);
+  const longitude = degreesLong(location.longitude);
+  const latitude = degreesLat(location.latitude);
 
   return (
-    <Marker
-      longitude={degreesLong(location.longitude)}
-      latitude={degreesLat(location.latitude)}
-      subpixelPositioning={true}
-    >
-      🛰️
-    </Marker>
+    <>
+      <Marker
+        longitude={longitude}
+        latitude={latitude}
+        subpixelPositioning={true}
+        onClick={onSatClick}
+        className="satmap-cursor-pointer hover:satmap-text-lg"
+      >
+        🛰️
+      </Marker>
+
+      {showPopup && (
+        <Popup
+          longitude={longitude}
+          latitude={latitude}
+          offset={[-10, 0]}
+          anchor="right"
+          subpixelPositioning={true}
+          onClose={() => setShowPopup(false)}
+        >
+          <div className="satmap-space-y-1">
+            <h4 className="satmap-m-0 satmap-text-center satmap-font-semibold">
+              {satellite.name}
+            </h4>
+            <ul className="satmap-list-none satmap-p-0 satmap-font-mono">
+              <li>lon: {longitude.toFixed(3)}</li>
+              <li>lat: {latitude.toFixed(3)}</li>
+              <li>alt: {location.height.toFixed(3)} km</li>
+            </ul>
+          </div>
+        </Popup>
+      )}
+    </>
   );
 }
