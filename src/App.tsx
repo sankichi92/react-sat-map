640f4f4288173542c9562e08b7fa53269868e74b
Takahiro Miyoshi <takahiro-miyoshi@sankichi.net>
2024-12-22 23:52:00 +0900

    Add satellites.txt
    from https://celestrak.org/NORAD/elements/gp.php?GROUP=last-30-days&FORMAT=tle

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,16 @@
 import { Map } from "react-map-gl/maplibre";
-import { SatelliteMarkers } from "../lib/main";
+import { Satellite, SatelliteMarkers } from "../lib/main";
 
 import "maplibre-gl/dist/maplibre-gl.css";
 import "../lib/main.css";
+import satellitesTxt from "./satellites.txt?raw";
 
-const satellites = [
-  {
-    name: "YODAKA",
-    tle: {
-      line1:
-        "1 62295U 98067XB  24356.80692813  .00138458  00000+0  20531-2 0  9992",
-      line2:
-        "2 62295  51.6364 105.6293 0012447   6.1035 354.0105 15.54174727  1956",
-    },
-  },
-  {
-    name: "ONGLAISAT",
-    tle: {
-      line1:
-        "1 62299U 98067XF  24356.80956985  .00137277  00000+0  20793-2 0  9997",
-      line2:
-        "2 62299  51.6370 105.6484 0011002   0.4931 359.6068 15.53614663  1919",
-    },
-  },
-];
+const lines = satellitesTxt.trim().split("\n");
+const satellites: Satellite[] = [];
+for (let i = 0; i < lines.length; i += 3) {
+  const [name, line1, line2] = lines.slice(i, i + 3);
+  satellites.push({ name, tle: { line1, line2 } });
+}
 
 export default function App() {
   return (
